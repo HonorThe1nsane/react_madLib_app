dc4a90ec8d86b915f9fc5a9a488e7015d7a0ab69
HonorThe1nsane <troymd2009@hotmail.com>
2023-10-14 15:00:16 -0600

    tried changing filename

--- mad_lib_app/zooML.js
+++ mad_lib_app/zooML.js
@@ -4,22 +4,20 @@ import { View, Text, StyleSheet } from 'react-native';
 const GoToZoo = () => {
     const [fileContent, setFileContent] = useState('');
 
-    useEffect(() => {
-        // Replace 'zooMadLib.txt' with the path to your text file
-        fetch('./https://github.com/HonorThe1nsane/react_madLib_app/blob/main/mad_lib_app/zooMadLib.txt')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch file');
-                }
-                return response.text();
-            })
-            .then((text) => {
-                setFileContent(text);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
+    fetch('./zooTxt.txt')
+    .then((response) => {
+        if (!response.ok) {
+            console.error('Response not OK:', response.status, response.statusText);
+            throw new Error('Network request failed');
+        }
+        return response.text();
+    })
+    .then((text) => {
+        setFileContent(text);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 
     return (
         <View style={styles.container}>
