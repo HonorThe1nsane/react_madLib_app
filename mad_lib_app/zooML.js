7f514000dcf559c6ed866c64e1e6b900d130b24c
HonorThe1nsane <troymd2009@hotmail.com>
2023-10-14 16:30:07 -0600

    update file to json

--- mad_lib_app/zooML.js
+++ mad_lib_app/zooML.js
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 const GoToZoo = () => {
-    const [fileContent, setFileContent] = useState('');
+    const [jsonData, setJsonData] = useState(null);
 
-    fetch('./zooTxt.txt')
-    .then((response) => {
-        if (!response.ok) {
-            console.error('Response not OK:', response.status, response.statusText);
-            throw new Error('Network request failed');
+    useEffect(() => {
+        async function readJsonFile() {
+            try {
+                const fileUri = './MadLib.json'; // Adjust the file name as needed
+                const fileInfo = await FileSystem.getInfoAsync(fileUri);
+
+                if (fileInfo.exists) {
+                    const content = await FileSystem.readAsStringAsync(fileUri);
+                    setJsonData(JSON.parse(content));
+                } else {
+                    console.error('File does not exist.');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
         }
-        return response.text();
-    })
-    .then((text) => {
-        setFileContent(text);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+
+        readJsonFile();
+    }, []);
 
     return (
         <View style={styles.container}>
             <Text>Welcome to the Zoo!</Text>
-            <Text>File Content:</Text>
-            <Text>{fileContent}</Text>
+            {jsonData ? (
+                <View>
+                    <Text>JSON Data:</Text>
+                    <Text>{JSON.stringify(jsonData, null, 2)}</Text>
+                </View>
+            ) : (
+                <Text>Loading JSON data...</Text>
+            )}
         </View>
     );
 };
